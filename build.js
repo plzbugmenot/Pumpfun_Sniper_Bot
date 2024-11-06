1454f597dc11bdfb0a5cca9953120916d2c93cdd
plzbugmenot <pleasebugmenot.dev@gmail.com>
2024-11-07 06:51:02 +0900

    Update build.js

--- build.js
+++ build.js
@@ -2,11 +2,6 @@ const esbuild = require("esbuild");
 
 esbuild
   .build({
-    entryPoints: ["./src/autoSniper.ts", "./src/singleSniper.ts"], // Your main entry point
-    outdir: "dist", // Output file
-    bundle: true, // Bundle dependencies
-    minify: true, // Minify for production
-    platform: "node", // Target Node.js runtime
-    target: "node16", // Node version compatibility
+    
   })
   .catch(() => process.exit(1));
